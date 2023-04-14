76431cb44f4b84e09f9c22584cd2b02352badb63
Anas Samoudi <unlimiteddez@gmail.com>
2023-04-14 03:37:01 +0000

    fixed error in Input

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -18,7 +18,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
             "flex w-full font-sans rounded-2xl h-12 bg-slate-200 bg-transparent py-2 px-5 text-sm  dark:border-slate-700 dark:text-slate-50",
             "placeholder:font-sans placeholder:text-slate-400 ",
             "focus:outline-none focus:bg-white focus:ring-2 focus:ring-blue-600  dark:focus:ring-slate-400 dark:focus:ring-offset-slate-900",
-            { "valid:ring-2 valid:ring-blue-600": true },
+            { "valid:ring-2 valid:ring-blue-600": isValid },
             { "ring-2 ring-red-600": hasError },
             "disabled:cursor-not-allowed disabled:opacity-50",
             className
