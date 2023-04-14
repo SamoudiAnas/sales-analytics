225612d7e963cb69108cf6645bdbf5b4930012e6
Anas Samoudi <unlimiteddez@gmail.com>
2023-04-14 03:35:53 +0000

    added input

---
+++ src/components/common/Input.tsx
@@ -0,0 +1,40 @@
+import * as React from "react";
+
+import clsx from "clsx";
+import { Check, X } from "lucide-react";
+
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  hasError?: boolean;
+  isValid?: boolean;
+}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, hasError, isValid = true, ...props }, ref) => {
+    return (
+      <div className="relative w-full">
+        <input
+          className={clsx(
+            "flex w-full font-sans rounded-2xl h-12 bg-slate-200 bg-transparent py-2 px-5 text-sm  dark:border-slate-700 dark:text-slate-50",
+            "placeholder:font-sans placeholder:text-slate-400 ",
+            "focus:outline-none focus:bg-white focus:ring-2 focus:ring-blue-600  dark:focus:ring-slate-400 dark:focus:ring-offset-slate-900",
+            { "valid:ring-2 valid:ring-blue-600": true },
+            { "ring-2 ring-red-600": hasError },
+            "disabled:cursor-not-allowed disabled:opacity-50",
+            className
+          )}
+          ref={ref}
+          {...props}
+        />
+        <div className="absolute top-1/2 right-4 -translate-y-1/2 flex gap-2">
+          {isValid && <Check className="text-blue-600" />}
+          {hasError && <X className="text-red-600" />}
+        </div>
+      </div>
+    );
+  }
+);
+
+Input.displayName = "Input";
+
+export { Input };
---
+++ src/components/pages/Auth/signup.page.tsx
@@ -0,0 +1,8 @@
+import { NextPage } from "next";
+import React from "react";
+
+const SignUpPage: NextPage = () => {
+  return <div>signup.page</div>;
+};
+
+export default SignUpPage;
